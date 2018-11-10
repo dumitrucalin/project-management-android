fe3cf2a737f74ef444b803b8ac71dbe1d4848a91
mariusAlc <mannye.skps@gmail.com>
2018-11-10 03:21:47 +0200

    Dashboard actual

--- app/src/main/java/com/example/calin/task_manager/SignUpActivity.java
+++ app/src/main/java/com/example/calin/task_manager/SignUpActivity.java
@@ -41,14 +41,8 @@ import userRoutes.SignUp;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-/**
- * A login screen that offers login via email/password.
- */
 public class SignUpActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
@@ -58,9 +52,7 @@ public class SignUpActivity extends AppCompatActivity implements LoaderCallbacks
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
+
     private UserLoginTask mAuthTask = null;
 
     // UI references.
@@ -163,9 +155,6 @@ public class SignUpActivity extends AppCompatActivity implements LoaderCallbacks
         return false;
     }
 
-    /**
-     * Callback received when a permissions request has been completed.
-     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
@@ -176,12 +165,6 @@ public class SignUpActivity extends AppCompatActivity implements LoaderCallbacks
         }
     }
 
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     private void attemptSignUp() {
         if (mAuthTask != null) {
             return;
@@ -268,9 +251,6 @@ public class SignUpActivity extends AppCompatActivity implements LoaderCallbacks
         return Validator.alphaNumericValidate(userPassword);
     }
 
-    /**
-     * Shows the progress UI and hides the login form.
-     */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
         // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
@@ -358,10 +338,6 @@ public class SignUpActivity extends AppCompatActivity implements LoaderCallbacks
         int IS_PRIMARY = 1;
     }
 
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
